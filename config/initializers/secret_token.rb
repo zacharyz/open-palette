bfe3100b8d09dc948e7664c281f433c577e36a9a
Zachary Zadell <zacharyz@gmail.com>
2011-05-02 13:16:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenPalette::Application.config.secret_token = '14df466d14ec41d2abc3a872bd11eb5b95de1c7afd695ccea51c41cda4215cda999ec0518566215b5d541aff736582b4ce57d449c9857402012f37b08725b70a'
