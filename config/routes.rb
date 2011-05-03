02186395865d94d1ccb2064fe08734359c360536
Zachary Zadell <zacharyz@gmail.com>
2011-05-03 16:03:57 -0700

    done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 OpenPalette::Application.routes.draw do
-  get "users/new"
 
   match '/contact', :to => 'pages#contact' 
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'   
-  match '/signup',    :to => 'users#new' 
+
 
   resources :microposts
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -89,6 +89,10 @@ describe UsersController do
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to open palette/i
+      end
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end    
     end
   end
