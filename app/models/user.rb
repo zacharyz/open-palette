02a85a514806b15d3ccb5c7a87674c55cc6c2f47
Zachary Zadell <zacharyz@gmail.com>
2011-05-03 15:15:49 -0700

    user signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 	validates :name,  :presence => true,
                       :length   => { :maximum => 50 }
-  validates :email, :presence => true
   validates :password, :presence     => true,
                          :confirmation => true,
                          :length       => { :within => 6..40 }
@@ -24,8 +23,6 @@ class User < ActiveRecord::Base
 	has_many :microposts
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name,  :presence => true,
-                    :length   => { :maximum => 50 }
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
