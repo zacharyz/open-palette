808a6a693853eb46f0ba1a8a41d9ca1408a51d26
Zachary Zadell <zacharyz@gmail.com>
2011-06-10 11:35:05 -0700

    old test files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -8,8 +8,8 @@ describe "posts" do
     fill_in :email,    :with => user.email
     fill_in :password, :with => user.password
     click_button
-  end
   
+  end
   describe "creation" do
     
     describe "failure" do
