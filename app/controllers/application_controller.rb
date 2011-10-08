f3afec8099d411cd79430711c94eb85b9ba772d4
Zachary Zadell <zacharyz@gmail.com>
2011-10-07 23:27:56 -0700

    styled sign in, sign up, and settings you can now cancel out of settings made branding link to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  before_filter :catch_cancel, :update => [:create, :update, :destroy]
+
+  private
+
+    def catch_cancel
+      redirect_back_or root_path if params[:commit] == "Cancel"
+    end
 end
