640184f84aa67c23c8ce655093217bac4d046bad
Zachary Zadell <zacharyz@gmail.com>
2011-10-14 01:32:27 -0700

    migrate post count on migrate

--- db/migrate/20111014002626_add_posts_count_to_user.rb
+++ db/migrate/20111014002626_add_posts_count_to_user.rb
@@ -1,5 +1,8 @@
 class AddPostsCountToUser < ActiveRecord::Migration
   def change
     add_column :users, :posts_count, :integer, :default => 0
-  end
+    User.all.each do |u|
+         User.update_counters u.id, :posts_count => u.posts.count
+    end
+  end  
 end
