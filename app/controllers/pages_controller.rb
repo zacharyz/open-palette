979afccb7d30a17080d27840eb53d57d823a989c
Zachary Zadell <zacharyz@gmail.com>
2011-10-16 19:50:43 -0700

    limit the amount of related posts and feed tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_filter :new_post, :except => :destroy
   def home
     @title = "Home"                             
-    @tags = Post.tag_counts_on(:tags).sort_by(&:count).reverse
+    @tags = Post.tag_counts_on(:tags).limit(15).sort_by(&:count).reverse
     if signed_in?
       #@feed_items = current_user.feed.paginate(:page => params[:page],:per_page => 18)       
       @feed_items = current_user.feed.page(params[:page]).per(18)                             
